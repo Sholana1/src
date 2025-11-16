b8ff93cf369903a5ede0d8e8546dfb7ad12e2dd8
Sholana1 <shodunkedavid16@gmail.com>
2025-11-16 15:44:59 +0100

    feat: add redis rabbitmq, celery, flower.

--- backend/docker/local/fastapi/celery/flower/start.sh
+++ backend/docker/local/fastapi/celery/flower/start.sh
@@ -9,10 +9,10 @@ FLOWER_CMD="celery \
     flower \
     --address=0.0.0.0 \
     --port=5555 \
-    --basic_auth=${CELERY_FLOWER_BASIC_AUTH}:${CELERY_FLOWER_PASSWORD}"
+    --basic_auth=${CELERY_FLOWER_USER}:${CELERY_FLOWER_PASSWORD}"
 
 
 exec watchfiles \
-    --filter pthon \
+    --filter python \
     --ignore-paths '.venv,.git,__pycache__,*.pyc' \
     "${FLOWER_CMD}"
